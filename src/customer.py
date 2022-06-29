19575e130b3ee8f8aab428485b0f117b67be24a9
Jacob <jacob_greenhalgh@hotmail.com>
2022-06-29 10:55:34 +0100

    First Commit

---
+++ src/customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self, _name, _wallet):
+        self.name = _name,
+        self.wallet = _wallet
+
+    def buy_drink(pub, drink):
+        self.pub.till += self.drink.price
+        
---
+++ src/pub.py
@@ -0,0 +1,8 @@
+class Pub:
+    
+    # Constructor
+    def __init__(self, _name, _till):
+        self.name = _name
+        self.till = _till
+        self.drinks = []
+    
